c26936315ffce909e5a4644e5d54a6815443c18d
venom7634 <venom7634@mail.ru>
2018-07-20 13:37:11 +0300

    Deleted unnecessary class

--- login-service/src/main/java/com/example/datausersservice/UsersServiceClient.java
+++ login-service/src/main/java/com/example/datausersservice/UsersServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(serviceId = "users-service", configuration = UsersServiceConfiguration.class)
+@FeignClient(serviceId = "users-service")
 public interface UsersServiceClient {
 
     @RequestMapping(value = "/users/getUserByLogin", method = RequestMethod.POST)
